ae3cb9f53c3be4209b05de079c2f463398b74a2f
ravirt7911 <ravirt7911@gmail.com>
2023-06-03 11:20:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,149 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState, useEffect } from "react";
+import "./App.css";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+} from "firebase/auth";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  serverTimestamp,
+} from "firebase/firestore";
+import { query, onSnapshot, orderBy } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDsyfYJVnZUNgjlSnlEstbeBxi2R7Kai-8",
+  authDomain: "chatellite-90213.firebaseapp.com",
+  projectId: "chatellite-90213",
+  storageBucket: "chatellite-90213.appspot.com",
+  messagingSenderId: "6843259055",
+  appId: "1:6843259055:web:af8e2b9ff43d3cfd695c5a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+const analytics = getAnalytics(app);
 
 function App() {
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Chatellite</h1>
+        <SignOut />
       </header>
+
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
+  );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // Handle successful sign-in
+      })
+      .catch((error) => {
+        // Handle sign-in error
+      });
+  };
+
+  return <button onClick={signInWithGoogle}>Sign in with Google</button>;
+}
+
+function SignOut() {
+  return (
+    auth.currentUser && (
+      <button className="sign-out" onClick={() => signOut(auth)}>
+        Sign Out
+      </button>
+    )
+  );
+}
+
+function ChatRoom() {
+  const dummy = useRef();
+  const messagesRef = collection(firestore, "messages");
+  const q = query(messagesRef, orderBy("createdAt"));
+
+  const [messages] = useCollectionData(q, { idField: "id" });
+
+  const [formValue, setFormValue] = useState("");
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await addDoc(messagesRef, {
+      text: formValue,
+      createdAt: serverTimestamp(),
+      uid,
+      photoURL,
+    });
+
+    setFormValue("");
+    dummy.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(q, () => {
+      dummy.current.scrollIntoView({ behavior: "smooth" });
+    });
+
+    return () => unsubscribe();
+  }, [q]);
+
+  return (
+    <>
+      <main>
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+
+        <span ref={dummy}></span>
+      </main>
+
+      <form onSubmit={sendMessage}>
+        <input
+          value={formValue}
+          onChange={(e) => setFormValue(e.target.value)}
+          placeholder="Say Anything!"
+        />
+
+        <button type="submit" disabled={!formValue}>
+          📤
+        </button>
+      </form>
+    </>
+  );
+}
+
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
+
+  return (
+    <>
+      <div className={`message ${messageClass}`}>
+        <img src={photoURL} alt="User" />
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
 
